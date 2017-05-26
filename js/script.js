d25ba9c6b5e5eacf803c54ef5dceaab92008c708
iastanley <iasvks.craigslist@gmail.com>
2017-05-25 17:08:42 -0700

    added smooth scrolling behavior to sidebar links

--- js/script.js
+++ js/script.js
@@ -4,4 +4,17 @@ $(document).ready(function(){
     $('#wrapper').toggleClass("toggled");
     $('#menu-toggle').toggleClass("toggled");
   });
+
+  //adding smooth scrolling to sidebar links
+  $('#sidebar-wrapper a').on('click', function(event) {
+    if (this.hash) {
+      event.preventDefault();
+      var hash = this.hash;
+      $('body, html').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 });
